4cacd32ae0f8960b6bd3edc7dfecea5d0b15822f
bachar-alhamada <bachar.alhamada76@gmail.com>
2023-12-14 21:31:44 -0600

    fgff

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,48 +1,47 @@
-'use strit'
-const figure = document.querySelector('figure');
-const mapbox = document.getElementById('map-container');   
-const icon = document.querySelector('.icon');
-const modal = document.getElementById('modal');
-const closeButton = document.querySelector('.close-button');
+'use strict';
 
+window.onload = function() {
+    const figure = document.querySelector('figure');
+    const mapbox = document.getElementById('map-container');
+    const icon = document.querySelector('.icon');
+    const modal = document.getElementById('modal');
+    const closeButton = document.querySelector('.close-button');
 
-let start = 0;
-function slider() {
-    start < slides.length ? start = start + 1  :  start = 1; 
-    figure.innerHTML = "<img src='./assets/image/" + slides[start-1] + "'>";
-}
-setInterval(slider, 2000);
+    setInterval(slider, 2000);
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYmFjaGFyYWxoYW1hZGEiLCJhIjoiY2xxNXpkNDN4MG5vdjJrbXg4ZGxqc2ExbSJ9.YU3s-nwlVBqMAGduw-EWMg';
+    mapbox.accessToken = 'pk.eyJ1IjoiYmFjaGFyYWxoYW1hZGEiLCJhIjoiY2xxNjEwZWhuMG5pMzJscGUxbHZmNTY1cyJ9.WVtKt8B3LU2cCkIz7Ln5vw';
 
-function makeMap(longitude, latitude) {
-    var map = new mapboxgl.Map({
-        container: 'map-container',
-        style: 'mapbox://styles/mapbox/streets-v12',
-        center: [longitude, latitude],
-        zoom: 15,
+    function makeMap(longitude, latitude) {
+        let map = new mapbox.Map({
+            container: 'map-container',
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [longitude, latitude],
+            zoom: 15
+        });
+
+        new mapbox.Marker()
+            .setLngLat([longitude, latitude])
+            .addTo(map);
+    }
+
+    closeButton.addEventListener('click', () => {
+        modal.style.display = 'none';
     });
 
-    new mapboxgl.Marker()
-        .setLngLat([longitude, latitude])
-        .addTo(map);
-}
-
-// Modal close event
-closeButton.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
-
-// Geolocation event
-icon.addEventListener('click', () => {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            function getLocation(position) {
-                const longitude = position.coords.longitude;
-                const latitude = position.coords.latitude;
-                makeMap(longitude, latitude); 
-            });
-    } else {
-        modal.style.display = 'block';
-    }
-});
+    icon.addEventListener('click', () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                function getLocation(position) {
+                    const longitude = position.coords.longitude;
+                    const latitude = position.coords.latitude;
+                    makeMap(longitude, latitude);
+                },
+                function error() {
+                    modal.style.display = 'block';
+                }
+            );
+        } else {
+            modal.style.display = 'block';
+        }
+    });
+};
